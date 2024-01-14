c8dab53893e9934178feb253ac712ceb62af5397
Simran-Parmar <simranparmar3220034@gmail.com>
2024-01-14 22:35:00 +0530

    Adding The First Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,90 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <div class="h-screen flex justify-center">
+      <div class="absolute flex justify-items-center w-5/6 h-2/4 rounded-xl mt-20 bg-slate-500">
+          <img src="https://images.unsplash.com/photo-1457369804613-52c61a468e7d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dmludGFnZSUyMGJvb2t8ZW58MHx8MHx8fDA%3D"
+              alt="Online Image" class="w-full object-cover rounded-xl">
+          <div
+              class="absolute mt-4 flex justify-center items-center text-6xl bg-opacity-30 backdrop-blur-sm px-6 text-black font-bold">
+              Discovre,<br> Share, and <br> Inspire, <br> TechTales.
+          </div>
+      </div>
+      <p class="absolute mt-96 py-20 text-2xl font-serif w-5/6">
+          TechTales is a blogging platform exclusively for tech enthusiasts. Share your innovative ideas, discover the
+          latest tech trends, and engage in insightful conversations with our global community.
+      </p>
+  </div>
+
+  <div class="h-screen container bg-neutral-200 flex flex-col items-center">
+
+      <p class="text-xl  flex justify-center font-sans mt-20">
+          Trusted by tech enthusiasts and novices from across the globe.
+      </p>
+
+      <div class="flex justify-around w-5/6 py-10">
+          <span class="text-4xl">Insights</span>
+          <span class="text-4xl">Knowledge</span>
+          <span class="text-4xl">Collabration</span>
+          <span class="text-4xl">Innovation</span>
+          <span class="text-4xl">Trends</span>
+      </div>
+
+      <div class="flex justify-between w-5/6 py-10">
+          <div>
+              <p class="text-6xl font-bold font-sans">
+                  Empowering Your <br> Tech Dialogue In <br> Multiple Ways
+              </p>
+          </div>
+          <p class="text-xl">
+              TechTale provides an intricate platform to share, discover and <br> inspire others with tech ideas. Our
+              platform streamlines reading and <br> sharing of tech blogs, providing insightful and intriguing content
+              for <br> tech enthusiasts worldwide.Empower your tech dialogue with our<br> user-friendly features.
+          </p>
+          <div>
+
+          </div>
+
+      </div>
+      <div class="mt-4 flex justify-around w-full font-serif space-x-4">
+          <div class="w-64 h-52 bg-white text-center">
+              <p class="text-sm py-12 font-bold ">
+                  Collaborative Writing
+              </p>
+              <p>
+                  Write, share, and co-author articles <br> with your peers.
+              </p>
+          </div>
+          <div class="w-64 h-52 bg-white text-center">
+              <p class="text-sm py-12 font-bold">
+                  Diverse Discussion Forums
+              </p>
+              <p>
+                  Engage in substantive conversations on technology trends.
+              </p>
+          </div>
+          <div class="w-64 h-52 bg-white text-center">
+              <p class="text-sm  py-12 font-bold">
+                  Personalized Feed
+              </p>
+              <p>
+                  Receive tailored content based on <br> your interests.s
+              </p>
+          </div>
+      </div>
+
+
+  </div>
+  <div class="h-screen container bg-white flex flex-col items-center">
+      <div class="flex text-xs font-mono justify-between w-5/6 py-20">
+          <div>
+          <span class="text-4xl">TechTales</span>
+          </div>
+          <div class="flex justify-around">
+          <span class="text-4xl">Youtube</span>
+          <span class="text-4xl mx-8">Twitter</span>
+          <span class="text-4xl">Linkdein</span>
+          </div>
+      </div>
+
+
+  </div>
+</div>
---
+++ src/routes/[username]/settings/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+    import { Separator } from "$lib/components/ui/separator";
+    import { Button, buttonVariants } from "$lib/components/ui/button";
+    import * as Dialog from "$lib/components/ui/dialog";
+    import { Input } from "$lib/components/ui/input";
+    import { Label } from "$lib/components/ui/label";
+    import * as Avatar from '$lib/components/ui/avatar';
+</script>
+
+<div>
+    <div class="space-y-1">
+      <h4 class="text-3xl font-medium leading-none mt-10 ">Personal Account</h4>
+    </div>
+    <Separator class="my-4" />
+    <div class="flex h-5 items-center space-x-4 text-lg">
+      <button>General</button>
+      <Separator orientation="vertical" />
+      <button>Security</button>
+      <Separator orientation="vertical" />
+      <button>Notifications</button>
+      <Separator orientation="vertical" />
+      <button>Sharing</button>
+    </div>
+  </div>
+
+  <div class="p-4">
+
+    <div class="flex items-center mb-4">
+      <div class="flex-1 mr-4">
+        <p class="text-2xl">Photo</p>
+      </div>
+      <div>
+        <button class="mb-2 px-4 py-2 bg-neutral-400 text-white rounded">Edit</button>
+      </div>
+    </div>
+    </div>
+  
+    <div class="p-4">
+
+      <div class="flex items-center mb-4">
+        <div class="flex-1 mr-4">
+          <p class="text-xl">Name</p>
+        </div>
+        <div class="flex items-center">
+          <p class="mr-2">User's Name</p> 
+          <button class="px-4 py-2 bg-neutral-400 text-white rounded">Edit</button>
+        </div>
+      </div>
+    
+      <div class="flex items-center">
+        <div class="flex-1 mr-4">
+          <p class="text-xl">Personal Email</p>
+        </div>
+        <div class="flex items-center">
+          <p class="mr-2">user@example.com</p> 
+          <button class="px-4 py-2 bg-neutral-400 text-white rounded">Edit</button>
+        </div>
+      </div>
+    
+    </div>
