3c7d99d6144614a16ad0372153cc5629a34c55c6
149segolte <37300847+149segolte@users.noreply.github.com>
2024-01-25 17:45:59 +0100

    Add dark mode selector

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { Frame, User, Settings, LogOut } from 'lucide-svelte';
+	import { Frame, User, Settings, LogOut, Sun, Moon } from 'lucide-svelte';
 	import * as Avatar from '$lib/components/ui/avatar';
 	import { Button } from '$lib/components/ui/button';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import type { PageData } from './$types';
+	import { ModeWatcher, setMode, resetMode } from 'mode-watcher';
 
 	export let data: PageData;
 	let workspace = data.workspace ? data.workspace : null;
 </script>
 
+<ModeWatcher />
 <div class="min-h-screen m-0 p-0 bg-neutral-100 overflow-auto">
 	<div class="container">
 		<nav class="my-6 bg-white p-2 rounded-lg shadow">
@@ -24,6 +26,24 @@
 					<Button href={`/${workspace}`} variant="outline" class="ml-4">@{workspace}</Button>
 				{/if}
 				<div class="ml-auto flex space-x-4">
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger asChild let:builder>
+							<Button builders={[builder]} variant="outline" size="icon">
+								<Sun
+									class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+								/>
+								<Moon
+									class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+								/>
+								<span class="sr-only">Toggle theme</span>
+							</Button>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content align="end">
+							<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+							<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+							<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
 					<DropdownMenu.Root>
 						<DropdownMenu.Trigger>
 							<Avatar.Root class="hover:cursor-pointer hover:drop-shadow">
